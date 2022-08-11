5aa99e2cd317ecb759c6dcbb21eb3226af8db93c
daichinagano <d.nagano1002@gmail.com>
2022-08-12 07:25:30 +0900

    ✨ add connect database

---
+++ constant/database.go
@@ -0,0 +1,9 @@
+package constant
+
+const (
+	DBHostEnv     = "DB_HOST"
+	DBPortEnv     = "DB_PORT"
+	DBNameEnv     = "DB_NAME"
+	DBUserEnv     = "DB_USER"
+	DBPasswordEnv = "DB_PASSWORD"
+)
---
+++ infra/database.go
@@ -0,0 +1,56 @@
+package infra
+
+import (
+	"fmt"
+	"log"
+
+	"clean-architecture/constant"
+	"clean-architecture/domain"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type GormHandler struct {
+	db *gorm.DB
+}
+
+func NewGormHandler() *GormHandler {
+	conn, err := connectDB()
+	if err != nil {
+		panic(err.Error)
+	}
+	GormHandler := new(GormHandler)
+	GormHandler.db = conn
+	return GormHandler
+}
+
+func connectDB() (*gorm.DB, error) {
+
+	dbHost := viper.GetString(constant.DBHostEnv)
+	dbPort := viper.GetString(constant.DBPortEnv)
+	dBName := viper.GetString(constant.DBNameEnv)
+	dbUser := viper.GetString(constant.DBUserEnv)
+	dbPassword := viper.GetString(constant.DBPasswordEnv)
+
+	dbAuth := dbUser
+	if len(dbPassword) > 0 {
+		dbAuth += ":" + dbPassword
+	}
+	dsn := fmt.Sprintf("%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local&timeout=10s", dbAuth, dbHost, dbPort, dBName)
+	var db *gorm.DB
+	var err error
+
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&domain.User{})
+	return db, nil
+}
+
+func (handler *GormHandler) Find(out interface{}, where ...interface{}) *gorm.DB {
+	return handler.db.Find(out, where...)
+}
